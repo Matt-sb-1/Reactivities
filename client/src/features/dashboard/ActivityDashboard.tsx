a0ae191d33862f6f876e82fb042b4de76f0f1414
Matt Jones <mjmattjones42@gmail.com>
2025-09-02 19:01:56 -0500

    End of Section 8

--- client/src/features/dashboard/ActivityDashboard.tsx
+++ client/src/features/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,6 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilters from "./ActivityFilters";
 
 
 export default function ActivityDashboard() {
@@ -9,7 +10,7 @@ export default function ActivityDashboard() {
                 <ActivityList  />
             </Grid2>
             <Grid2 size={5}>
-                Activity filters go here
+                <ActivityFilters />
             </Grid2>
         </Grid2>
     )
