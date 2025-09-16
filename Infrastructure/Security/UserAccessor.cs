ec6004e78469f030260b9c63973fff578fb6b7e6
Matt Jones <mjmattjones42@gmail.com>
2025-09-16 18:11:10 -0500

    End of section 14

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Security.Claims;
+using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Persistence;
+
+namespace Infrastructure.Security;
+
+public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext dbContext)
+    : IUserAccessor
+{
+    public async Task<User> GetUserAsync()
+    {
+        var id = GetUserId();
+        return await dbContext.Users.FindAsync(id)
+            ?? throw new UnauthorizedAccessException("No user is logged in");
+    }
+
+    public string GetUserId()
+    {
+        return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("No user found");
+    }
+}
